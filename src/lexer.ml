1cdd206b2d513497d28d6682c048679da301cf31
Martin Janiczek <martin@janiczek.cz>
2022-12-27 01:07:53 +0100

    Implement shebang

--- src/lexer.ml
+++ src/lexer.ml
@@ -1,6 +1,8 @@
-let digit = [%sedlex.regexp? '0' .. '9']
+let digit  = [%sedlex.regexp? '0' .. '9']
 let number = [%sedlex.regexp? Plus digit]
-let lineComment = [%sedlex.regexp? "//", Star (Compl '\n'), '\n' ]
+let untilEndOfLine = [%sedlex.regexp? Star (Compl '\n'), '\n']
+let lineComment    = [%sedlex.regexp? "//", untilEndOfLine]
+let shebangComment = [%sedlex.regexp? "#!", untilEndOfLine]
 
 open Parser
 
@@ -10,6 +12,7 @@ let rec token buf =
   match%sedlex buf with
   | Plus (Chars " \t") -> token buf
   | lineComment -> token buf
+  | shebangComment -> token buf (* TODO this means it can happen anywhere, but we'd only like it to be allowed on the first line *)
   | '\n' -> EOL
   | number -> INT (int_of_string (Sedlexing.Latin1.lexeme buf))
   | '+' -> PLUS
