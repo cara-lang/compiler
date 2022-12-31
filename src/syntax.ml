fc2b904cc98fe846f7a31ab180d2bb1f9a1b9e08
Martin Janiczek <martin@janiczek.cz>
2022-12-31 13:02:09 +0100

    Throw an error when mixing holes

--- src/syntax.ml
+++ src/syntax.ml
@@ -69,46 +69,48 @@ let add map k v =
     | `Ok new_map -> new_map
     | `Duplicate -> failwith "Map add - duplicate shouldn't happen"
 
-let rec max_hole_n = function
+type holes =
+  | NoHoles
+  | OnlyNumbered of int (* max *)
+  | OnlyUnderscore
+  | Mixed
+
+let combine_holes h1 h2 = 
+  match (h1, h2) with
+    | (NoHoles, _) -> h2
+    | (_, NoHoles) -> h1
+    | (Mixed, _) -> h1
+    | (_, Mixed) -> h2
+    | (OnlyNumbered m1, OnlyNumbered m2) -> OnlyNumbered (max m1 m2)
+    | (OnlyNumbered _, OnlyUnderscore) -> Mixed
+    | (OnlyUnderscore, OnlyNumbered _) -> Mixed
+    | (OnlyUnderscore, OnlyUnderscore) -> OnlyUnderscore
+
+let rec analyze_holes = function
   | EIdentifier ([],x) -> if Char.equal '_' (String.get x 0)
                           then if String.length x = 1
-                               then 1 (* _ *)
-                               else Int.of_string (String.drop_prefix x 1) (* _3 *)
-                          else 0
-  | EIdentifier _ -> 0
-  | EInt _    -> 0
-  | EFloat _  -> 0
-  | EChar _   -> 0
-  | EString _ -> 0
-  | EUnit     -> 0
-  | ETuple es -> es |> List.map ~f:max_hole_n |> List.max_elt ~compare |> Option.value ~default:0
-  | EList es  -> es |> List.map ~f:max_hole_n |> List.max_elt ~compare |> Option.value ~default:0
-  | ELambda (_,e)    -> max_hole_n e
-  | EClosure (_,e,_) -> max_hole_n e
-  | EUnOp (_,e)      -> max_hole_n e
-  | EBinOp (e1,_,e2) -> max (max_hole_n e1) (max_hole_n e2)
-  | ECall (fn,args)  -> max (max_hole_n fn) (args |> List.map ~f:max_hole_n |> List.max_elt ~compare |> Option.value ~default:0)
-
-let rec fix_unnumbered_hole e = match e with 
-  | EIdentifier ([],"_") -> EIdentifier ([],"_1")
-  | EIdentifier _ -> e
-  | EInt _    -> e
-  | EFloat _  -> e
-  | EChar _   -> e
-  | EString _ -> e
-  | EUnit     -> e
-  | ETuple es -> ETuple (es |> List.map ~f:fix_unnumbered_hole)
-  | EList es  -> EList  (es |> List.map ~f:fix_unnumbered_hole)
-  | ELambda (ns,b)    -> ELambda (ns,fix_unnumbered_hole b)
-  | EClosure (ns,b,d) -> EClosure (ns,fix_unnumbered_hole b,d)
-  | EUnOp (u,e)      -> EUnOp (u,fix_unnumbered_hole e)
-  | EBinOp (e1,b,e2) -> EBinOp (fix_unnumbered_hole e1,b,fix_unnumbered_hole e2)
-  | ECall (fn,args)  -> ECall (fix_unnumbered_hole fn,List.map ~f:fix_unnumbered_hole args)
+                               then OnlyUnderscore (* _ *)
+                               else OnlyNumbered (Int.of_string (String.drop_prefix x 1)) (* _3 *)
+                          else NoHoles
+  | EIdentifier _ -> NoHoles
+  | EInt _    -> NoHoles
+  | EFloat _  -> NoHoles
+  | EChar _   -> NoHoles
+  | EString _ -> NoHoles
+  | EUnit     -> NoHoles
+  | ETuple es -> es |> List.map ~f:analyze_holes |> List.fold ~init:NoHoles ~f:combine_holes
+  | EList es  -> es |> List.map ~f:analyze_holes |> List.fold ~init:NoHoles ~f:combine_holes
+  | ELambda (_,e)    -> analyze_holes e
+  | EClosure (_,e,_) -> analyze_holes e
+  | EUnOp (_,e)      -> analyze_holes e
+  | EBinOp (e1,_,e2) -> combine_holes (analyze_holes e1) (analyze_holes e2)
+  | ECall (fn,args)  -> combine_holes (analyze_holes fn) (args |> List.map ~f:analyze_holes |> List.fold ~init:NoHoles ~f:combine_holes)
 
 let lambda_with_holes body =
-  (* TODO throw a fit if user is mixing _ and _1,_2,... *)
-  let max_hole = max_hole_n body in
-  let params = List.range 1 (max_hole + 1) |> List.map ~f:(fun i -> "_" ^ Int.to_string i) in
-  (* Thanks to already representing the holes as EIdentifiers, we only need to change "_" into "_1" and the rest will all line up! *)
-  let new_body = fix_unnumbered_hole body in
-  ELambda (params,new_body)
+  match analyze_holes body with
+    | NoHoles -> ELambda ([], body)
+    | OnlyUnderscore -> ELambda (["_"], body)
+    | OnlyNumbered max_hole -> 
+        let params = List.range 1 (max_hole + 1) |> List.map ~f:(fun i -> "_" ^ Int.to_string i) in
+        ELambda (params, body)
+    | Mixed -> failwith "E0020: Anonymous function shorthand with mixed holes"
