28c37960183d3c131fc1f52e0226f18dfd44c2a8
Martin Janiczek <martin@janiczek.cz>
2023-02-16 21:34:48 +0100

    Remove the parser ML file, not needed anymore!

--- src/parser.ml
+++
@@ -1,7 +0,0 @@
-include Nice_parser.Make(struct
-  type result = Syntax.decl list
-  type token = Menhir_parser.token
-  exception ParseError = Menhir_parser.Error
-  let parse = Menhir_parser.main
-  include Lexer
-end)
